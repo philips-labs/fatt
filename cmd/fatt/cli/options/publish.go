6ad54295092de64f4170fd6be445dbf6b2df63e5
Marco Franssen <marco.franssen@philips.com>
2022-03-24 10:44:40 +0100

    Implement ociRef to publish towards by hardcoded convention

--- cmd/fatt/cli/options/publish.go
+++ cmd/fatt/cli/options/publish.go
@@ -8,6 +8,7 @@ import (
 type PublishOptions struct {
 	*OCIOptions
 	Version      string
+	Repository   string
 	Attestations []string
 }
 
@@ -21,5 +22,6 @@ var _ CommandFlagger = (*PublishOptions)(nil)
 // AddFlags implements CommandFlagger to add the RootOptions as flags to the given command
 func (o *PublishOptions) AddFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().StringVarP(&o.Version, "version", "", "", "the version to publish the attestations for.")
+	cmd.PersistentFlags().StringVarP(&o.Repository, "repository", "", "", "the oci repository to publish the attestation in.")
 	o.OCIOptions.AddFlags(cmd)
 }
---
+++ pkg/attestation/publish.go
@@ -0,0 +1,44 @@
+package attestation
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// PublishResult captures the result after publishing the attestations
+type PublishResult struct {
+	AttestationFile string
+	OCIRef          string
+}
+
+// Publish publishes the attestations to an oci repository
+func Publish(repository, version, attestationRef string) (*PublishResult, error) {
+	t, err := getType(attestationRef)
+	if err != nil {
+		return nil, err
+	}
+
+	ociRef := fmt.Sprintf("%s:%s.%s", repository, version, t)
+
+	fileName := strings.Split(attestationRef, "://")[1]
+
+	return &PublishResult{
+		AttestationFile: fileName,
+		OCIRef:          ociRef,
+	}, nil
+}
+
+func getType(attestationRef string) (string, error) {
+	prov := "provenance"
+	sbom := "sbom"
+
+	switch {
+	case strings.HasPrefix(attestationRef, prov+"://"):
+		return prov, nil
+	case strings.HasPrefix(attestationRef, sbom+"://"):
+		return sbom, nil
+	default:
+		return "", errors.New("could not parse attestation scheme, use <scheme>://<file> format")
+	}
+}
