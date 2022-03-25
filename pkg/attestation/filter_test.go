4c66e298e3c25832c98e2d9e5a3328fdc64ed3ca
Marco Franssen <marco.franssen@philips.com>
2022-03-25 17:12:25 +0100

    Fix filters #21

--- pkg/attestation/filter_test.go
+++ pkg/attestation/filter_test.go
@@ -11,12 +11,11 @@ import (
 )
 
 func TestReducePurls(t *testing.T) {
-
-	purlsFile := `pkg:docker/philips-labs/fatt@sha256:6cc65b2c82c2baa3391890abb8ab741efbcbc87baff3b06d5797afacb314ddd9?repository_url=ghcr.io&attestation_type=sbom
-pkg:docker/philips-labs/fatt@sha256:6cc65b2c82c2baa3391890abb8ab741efbcbc87baff3b06d5797afacb314ddd9?repository_url=ghcr.io&attestation_type=provenance
-pkg:docker/philips-labs/fatt@sha256:6cc65b2c82c2baa3391890abb8ab741efbcbc87baff3b06d5797afacb314ddd9?repository_url=ghcr.io
-pkg:nuget/philips-labs/fatt@sha256:6cc65b2c82c2baa3391890abb8ab741efbcbc87baff3b06d5797afacb314ddd9?repository_url=nuget.org&attestation_type=provenance
-pkg:nuget/philips-labs/fatt@sha256:823413cc65b2c82c2baa3391890abb8ab741e87baff3b06d5797afacb314ddd9?repository_url=nuget.org&attestation_type=sbom`
+	purlsFile := `pkg:docker/philips-labs/fatt@sha256:6cc65b2c82c2baa3391890abb8ab741efbcbc87baff3b06d5797afacb314ddd9?repository_url=ghcr.io%2Fphilips-labs%2Ffatt&tag=v0.1.0
+pkg:oci/philips-labs/fatt-cli@sha256:5f74adb7a68a89d96aa3571d93b2e96c68b29891e4c646e5d31f25b3d6eb308a?repository_url=ghcr.io%2Fphilips-labs%2Ffatt-cli&tag=v0.1.0.sbom
+pkg:oci/philips-labs/fatt-cli@sha256:bc59775e7ec4983e1b79aa0234ee1020f566eedf01b53e99d44645c12c402826?repository_url=ghcr.io%2Fphilips-labs%2Ffatt-cli&tag=v0.1.0.provenance
+pkg:oci/philips-labs/fatt-cli@sha256:5f74adb7a68a89d96aa3571d93b2e96c68b29891e4c646e5d31f25b3d6eb308a?repository_url=index.docker.io%2Fphilipssoftware%2Ffatt-cli&tag=v0.1.0.sbom
+pkg:oci/philips-labs/fatt-cli@sha256:bc59775e7ec4983e1b79aa0234ee1020f566eedf01b53e99d44645c12c402826?repository_url=index.docker.io%2Fphilipssoftware%2Ffatt-cli&tag=v0.1.0.provenance`
 
 	testCases := []struct {
 		name           string
@@ -24,12 +23,13 @@ pkg:nuget/philips-labs/fatt@sha256:823413cc65b2c82c2baa3391890abb8ab741e87baff3b
 		resultCount    int
 		expectedErrMsg string
 	}{
+		{name: "empty filter", filter: "", resultCount: 5},
 		{name: "sbom filter", filter: `{ .IsAttestationType("sbom") }`, resultCount: 2},
 		{name: "provenance filter", filter: `{ .IsAttestationType("provenance") }`, resultCount: 2},
-		{name: "empty filter", filter: "", resultCount: 5},
-		{name: "docker type filter", filter: `{ .PURL.Type == "docker" }`, resultCount: 3},
-		{name: "nuget filter", filter: `{ .IsRegistry("nuget.org") }`, resultCount: 2},
-		{name: "nuget sbom filter", filter: `{ .IsRegistry("nuget.org") && .IsAttestationType("sbom") }`, resultCount: 1},
+		{name: "docker type filter", filter: `{ .PURL.Type == "docker" }`, resultCount: 1},
+		{name: "oci type filter", filter: `{ .PURL.Type == "oci" }`, resultCount: 4},
+		{name: "nuget filter", filter: `{ .IsRegistry("index.docker.io") }`, resultCount: 2},
+		{name: "nuget sbom filter", filter: `{ .IsRegistry("index.docker.io") && .IsAttestationType("sbom") }`, resultCount: 1},
 		{name: "invalid filter", filter: `{ .Version == "v0.3.1" }`, resultCount: 0, expectedErrMsg: "cannot fetch Version from attestation.Attestation (1:25)\n | filter(Attestations, { .Version == \"v0.3.1\" })\n | ........................^"},
 		{name: "invalid expression", filter: "{}", resultCount: 0, expectedErrMsg: "unexpected token Bracket(\"}\") (1:23)\n | filter(Attestations, {})\n | ......................^"},
 	}
